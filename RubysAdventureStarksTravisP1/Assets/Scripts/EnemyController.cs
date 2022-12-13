a012791f56435ea67932f94ce7093bbd386dab9d
TravisDStarks <110067652+TravisDStarks@users.noreply.github.com>
2022-12-13 09:55:28 -0800

    W
    particles

--- RubysAdventureStarksTravisP1/Assets/Scripts/EnemyController.cs
+++ RubysAdventureStarksTravisP1/Assets/Scripts/EnemyController.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
-
 public class EnemyController : MonoBehaviour
 {
     public float speed;
     public bool vertical;
     public float changeTime = 3.0f;
+
+    public ParticleSystem smokeEffect;
 
     Rigidbody2D rigidbody2D;
     float timer;
@@ -81,5 +82,7 @@ public class EnemyController : MonoBehaviour
         rigidbody2D.simulated = false;
         //optional if you added the fixed animation
         animator.SetTrigger("Fixed");
+
+        smokeEffect.Stop();
     }
 }
